ff0d5b2ed6b01e7e976173bcb7e3184de19d4e22
wangrj6 <wangrj6@chinatelecom.cn>
2022-06-13 23:37:51 +0800

    Update

--- freemarker-test.sh
+++ freemarker-test.sh
@@ -13,8 +13,9 @@ cat >freemarker-test.txt <<'EOF'
 EOF
 
 # 从管道接收模板内容，并且通过参数来定义模板中需要替换的变量集合
-#   说明1：if指令的变量，只有值为"true"的情况下，if指令才会判断为真，其他任何值，都会视为false
-#   说明2：数组变量名是以[]作为后缀，[]内可以指定分隔符，默认分隔符为逗号
+#   说明1：模板中定义的变量名需要符合正在表达式：[a-zA-Z0-9_-]+
+#   说明2：if指令的变量，只有值为"true"的情况下，if指令才会判断为真，其他任何值，都会视为false
+#   说明3：数组变量名是以[]作为后缀，[]内可以指定分隔符，默认分隔符为逗号
 cat freemarker-test.txt | ./freemarker.pl name=wrj age=26 marry=true \
   hobbies['|']='run|swim' job="software engineer" es-servers[]='es-1,es-2,es-3'
 
